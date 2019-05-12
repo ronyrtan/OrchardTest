1d07a35b47693ef34af0f81bb35cb5ab0e74046b
ronyrtan <ron.yr.tan@hotmail.com>
2019-05-13 01:35:49 +1000

    Added capture function on anchor tags and updated styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+// Open and close the Image Modal
 var openModal = function(e) {
 
   var modal = document.getElementById('imgModal')
@@ -14,5 +14,10 @@ var openModal = function(e) {
   close.addEventListener("click", function() {
     modal.style.display = "none";
   });
+}
 
+// Capture Article Module clicks
+var capture = function(e) {
+  console.log('Anchor tag hrefs have been left blank since link to article pages were not provided - can be linked to any page necessary such as a new article page');
+  console.log(e);
 }
